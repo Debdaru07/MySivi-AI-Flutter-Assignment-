718099bae6b14de7c4a09e755d24c3b8d5072619
Debdaru Dasgupta <debdarudasgupta0799@gmail.com>
2025-12-27 14:57:20 +0530

    random API integrated

--- lib/features/chat/data/chat_api_service.dart
+++ lib/features/chat/data/chat_api_service.dart
@@ -1,11 +1,38 @@
 import 'dart:convert';
+import 'dart:developer' as console;
+import 'dart:math';
 import 'package:http/http.dart' as http;
 import '../../../core/constants/api_constants.dart';
 
 class ChatApiService {
+  final _random = Random();
+
   Future<String> fetchMessage() async {
-    final res = await http.get(Uri.parse(ApiConstants.randomQuote));
-    final data = jsonDecode(res.body);
-    return data['content'];
+    try {
+      console.log('url - ${ApiConstants.comments}');
+
+      final res = await http.get(Uri.parse(ApiConstants.comments));
+
+      if (res.statusCode != 200) {
+        throw Exception('Network error');
+      }
+
+      final data = jsonDecode(res.body);
+      console.log('data - $data');
+
+      if (data is Map && data['comments'] is List) {
+        final List comments = data['comments'];
+
+        if (comments.isNotEmpty) {
+          final randomIndex = _random.nextInt(comments.length);
+          return comments[randomIndex]['body'] ?? '';
+        }
+      }
+
+      throw Exception('Invalid API response');
+    } catch (exc) {
+      console.log('exception - $exc');
+      throw Exception('Invalid API response');
+    }
   }
 }
